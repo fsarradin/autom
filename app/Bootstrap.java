64278403007819809eda6a8e4ca5e7d5f6a1c089
Francois Sarradin <fsarradin@gmail.com>
2011-05-09 13:59:25 +0200

    add bootstrap to load test data

---
+++ app/Bootstrap.java
@@ -0,0 +1,21 @@
+import models.User;
+import play.Logger;
+import play.Play;
+import play.jobs.Job;
+import play.jobs.OnApplicationStart;
+import play.test.Fixtures;
+
+@OnApplicationStart
+public class Bootstrap extends Job {
+
+    @Override
+    public void doJob() {
+        if (Play.mode == Play.Mode.DEV) {
+            if (User.count() == 0) {
+                Logger.info("DEV mode: load data...");
+                Fixtures.loadModels("test-data.yml");
+            }
+        }
+    }
+
+}
